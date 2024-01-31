9e95c0a8f77c48e34e8d6636aa3e4b6a7f2a76b8
loydcardozo <loyd.cardozo@gmail.com>
2024-02-01 00:50:40 +0530

    onboarding error solved

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 // import 'package:medapp/pages/homepage.dart';
 import 'package:medapp/pages/onboarding_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:device_info_plus/device_info_plus.dart';
+import 'package:medapp/pages/loginpage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,8 +16,46 @@ const MyApp({Key? key}) : super(key: key);
 Widget build(BuildContext context) {
   return MaterialApp(
      debugShowCheckedModeBanner: false,
-    home:  OnboardingScreen(),
       theme: ThemeData(brightness: Brightness.dark),
+    home:  FutureBuilder<bool>(
+     future: shouldShowOnboarding(getInstallationId),
+     builder: (context, snapshot) {
+      if (snapshot.connectionState == ConnectionState.waiting) {
+        return CircularProgressIndicator();
+      } else if (snapshot.data == true) {
+        return OnboardingScreen();
+      } else {
+        return LoginPage();
+      }
+     }  
+    )
   );
 }
+}
+
+Future<bool> shouldShowOnboarding(Function getInstallationId) async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  String storedInstallationId = prefs.getString('installationId') ?? '';
+  String currentInstallationId = await getInstallationId();
+
+  if (storedInstallationId.isEmpty || storedInstallationId != currentInstallationId) {
+   
+    await prefs.setString('installationId', currentInstallationId);
+    await prefs.setBool('firstLaunch', true);
+
+    return true;
+  } else {
+    bool isFirstLaunch = prefs.getBool('firstLaunch') ?? true;
+    return isFirstLaunch;
+  }
+}
+
+Future<String> getInstallationId(BuildContext context) async {
+  DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
+  if (Theme.of(context).platform == TargetPlatform.android) {
+    AndroidDeviceInfo androidInfo = await deviceInfo.androidInfo;
+    return androidInfo.model;
+  } else {
+    return '';
+  }
 }
